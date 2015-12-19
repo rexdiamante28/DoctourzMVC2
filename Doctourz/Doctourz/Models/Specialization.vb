50f2f53c3e775fb9b3bd196eaba3129fc776a5bc
kpnava <kennethpaulnava@gmail.com>
2015-12-19 17:17:47 +0800

    commit models for doctor

---
+++ Doctourz/Doctourz/Models/Specialization.vb
@@ -0,0 +1,49 @@
+﻿Imports System.ComponentModel.DataAnnotations
+Imports System.ComponentModel.DataAnnotations.Schema
+
+Public Class Specialization
+    Private _specializationId As Integer
+    <Key>
+    <DatabaseGenerated(DatabaseGeneratedOption.Identity)>
+    Public Property NewProperty() As Integer
+        Get
+            Return _specializationId
+        End Get
+        Set(ByVal value As Integer)
+            _specializationId = value
+        End Set
+    End Property
+
+    Private _name As String
+    Public Property name() As String
+        Get
+            Return _name
+        End Get
+        Set(ByVal value As String)
+            _name = value
+        End Set
+    End Property
+
+    Private _description As String
+    Public Property description() As String
+        Get
+            Return _description
+        End Get
+        Set(ByVal value As String)
+            _description = value
+        End Set
+    End Property
+
+    Private _userId As String
+    Public Property userId() As String
+        Get
+            Return _userId
+        End Get
+        Set(ByVal value As String)
+            _userId = value
+        End Set
+    End Property
+
+    Public Overridable Property AppUser() As AppUsers
+
+End Class
