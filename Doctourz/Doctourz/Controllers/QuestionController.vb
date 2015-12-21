b384cf9e9829c41f6b3358613c45ed60ee485b98
kpnava <kennethpaulnava@gmail.com>
2015-12-21 12:01:20 +0800

    commit question and rtc

---
+++ Doctourz/Doctourz/Controllers/QuestionController.vb
@@ -0,0 +1,88 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class QuestionController
+        Inherits Controller
+
+        'COMMENT
+        ' GET: Question
+        Function Main() As ActionResult
+            Return View()
+        End Function
+
+        ' GET: Question/Details/5
+        Function Details(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        ' GET: Question/Create
+        Function AddTraits() As ActionResult
+            Return PartialView("AddTraits")
+        End Function
+
+        ' POST: Question/Create
+        <HttpPost()>
+        Function AddTraits(ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add insert logic here
+                Dim db = New ApplicationDbContext
+                Dim trait = New Traits With {
+                    .trait = collection.GetValue("trait").AttemptedValue,
+                    .description = collection.GetValue("description").AttemptedValue,
+                    .color = collection.GetValue("color").AttemptedValue
+                }
+                db.Traits.Add(trait)
+                db.SaveChanges()
+
+                Return RedirectToAction("ViewTraits")
+            Catch
+                Return View()
+            End Try
+        End Function
+
+        ' GET: Question/Edit/5
+        Function Edit(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        ' POST: Question/Edit/5
+        <HttpPost()>
+        Function Edit(ByVal id As Integer, ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add update logic here
+                ' TODO: Add update logic here
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function
+
+        ' GET: Question/Delete/5
+        Function DeleteTrait(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        ' POST: Question/Delete/5
+        <HttpPost()>
+        Function DeleteTrait(ByVal id As Integer, ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add delete logic here
+                Dim db = New ApplicationDbContext
+                Dim trait = db.Traits.Where(Function(model) model.traitId = id).First
+                db.Traits.Remove(trait)
+                db.SaveChanges()
+
+                Return RedirectToAction("ViewTraits")
+            Catch
+                Return View()
+            End Try
+        End Function
+
+        Function ViewTraits() As ActionResult
+            Dim db = New ApplicationDbContext
+            Dim model = db.Traits
+
+            Return View(model)
+        End Function
+    End Class
+End Namespace
---
+++ Doctourz/Doctourz/Controllers/RTCHub.vb
@@ -0,0 +1,96 @@
+﻿
+Imports Microsoft.AspNet.SignalR
+Imports Doctourz.opentokRTC.Models
+Imports System.Collections.Concurrent
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Threading.Tasks
+Imports System.Web
+
+Namespace opentokRTC.Controllers
+    <Hubs.HubName("rTCHub")>
+    Public Class RTCHub
+        Inherits Hub
+
+        Public Shared Users As New ConcurrentDictionary(Of String, User)()
+        Public Shared connections As New connections()
+
+
+        Public Function GetConnected(username As String, Remote_Address As String) As User
+            Dim user As User
+            connections.Add(Context.ConnectionId)
+            Dim ot = New oTok(Remote_Address)
+            user = New User(username, Context.ConnectionId, ot)
+
+            Users.TryAdd(Context.ConnectionId, user)
+            Clients.Clients(connections.AllExcept(Context.ConnectionId)).getNewOnlineUser(user)
+            Clients.Client(Context.ConnectionId).getOnlineUsers(Users)
+
+
+            Return user
+        End Function
+
+
+        Public Sub BeginCall(toConnectionId As String)
+
+            Dim touser = Users(toConnectionId)
+            Dim caller = Users(Context.ConnectionId)
+            Dim users__1 As New List(Of User)()
+
+            Clients.Client(toConnectionId).notifybeginCall(touser, caller)
+
+
+        End Sub
+
+        Public Sub CallRejectedSignal(CallerConnectionId As String)
+
+            Dim self = Users(Context.ConnectionId)
+
+            Clients.Client(CallerConnectionId).notifyCallrejected("Call rejected by : " + self.Name)
+        End Sub
+
+
+
+        Public Sub EndCall(CallerConnectionId As String)
+            Try
+
+
+                Dim caller As New User()
+                Dim self As New User()
+                If Users.ContainsKey(Context.ConnectionId) Then
+                    self = Users(Context.ConnectionId)
+                    caller = Users(CallerConnectionId)
+
+                    Clients.Client(CallerConnectionId).notifyCallend(caller, self)
+                End If
+            Catch exp As Exception
+            End Try
+        End Sub
+
+
+        Private Sub GetDisConnected(ConnectionId As String)
+            If Users.ContainsKey(ConnectionId) = True Then
+                Dim user As User
+                Dim currentuser As User = Users(ConnectionId)
+
+                Users.TryRemove(ConnectionId.ToString(), user)
+                connections.Remove(ConnectionId)
+                Clients.Clients(connections.AllExcept(ConnectionId)).disconnected(user)
+            End If
+        End Sub
+
+        Public Sub GetDisConnected()
+            GetDisConnected(Context.ConnectionId)
+        End Sub
+
+        Public Overrides Function OnDisconnected(stopcalled As Boolean) As Task
+            GetDisConnected(Context.ConnectionId)
+
+            Return MyBase.OnDisconnected(stopcalled)
+        End Function
+
+
+    End Class
+End Namespace
+
+
